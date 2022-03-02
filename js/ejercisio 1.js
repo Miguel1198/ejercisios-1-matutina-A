0740b5f059cf5a9559d0be2a5c70675c6f1bbed4
miguel <miguelbolanos021@gmail.com>
2022-03-02 07:39:53 -0500

    ejercisios

---
+++ js/ejercisio 1.js
@@ -0,0 +1,7 @@
+document.getElementById("tipo1").innerHTML = "ejemplo del tipo 1 de Output";
+
+document.write("Ejemplo de texto con document.write");
+
+alert("Ejemplo de output con window.alert");
+
+console.log("Hola Mundo");
